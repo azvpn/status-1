e559ee901fff4ca0a48a5973c618b091b72a5437
azvpn cute <106140258+azvpn@users.noreply.github.com>
2022-05-24 16:14:58 +0700

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Key: " key
 
-if [[ $key == key1 ]]; then
+if [[ $key == long ]]; then
 sleep 2
 echo "Xác Thực Thành Công !" 
 sleep 1
@@ -10,7 +10,10 @@ elif [[ $key == key2 ]]; then
 sleep 2
 echo "Xác Thực Thành Công !" 
 sleep 1
-
+elif [[ $key == key3 ]]; then
+sleep 2
+echo "Xác Thực Thành Công !" 
+sleep 1
 else 
 sleep 2
 echo "Key Không Chính Xác !"
