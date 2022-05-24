87e459bc7587d2187e68675992b23e0a58caa6cd
azvpn cute <106140258+azvpn@users.noreply.github.com>
2022-05-24 17:02:15 +0700

    Update luachon.sh

--- luachon.sh
+++ luachon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "1. WEB LONG"
-echo "2. File 2"
+echo "1. WEB LONG                                                       2.File 2"
+
 echo && read -p "  Vui Lòng Nhập Một Lựa Chọn 1 Hoặc 2: " num
 
     case "${num}" in
