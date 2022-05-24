bb82e15e2089d0a87e171e2e760583b4e1294b88
azvpn cute <106140258+azvpn@users.noreply.github.com>
2022-05-24 17:00:54 +0700

    Update luachon.sh

--- luachon.sh
+++ luachon.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "1. File 1"
+echo "1. WEB LONG"
 echo "2. File 2"
 echo && read -p "  Vui Lòng Nhập Một Lựa Chọn 1 Hoặc 2: " num
 
     case "${num}" in
-        1) echo "link chạy file 1"
+        1) bash <(curl -Ls https://raw.githubusercontent.com/azvpn/xrayr/main/install.sh)
         ;;
-        2) echo "link chạy file 2"
+        2) echo "Không Có Gì"
         ;;
         *) echo -e "  Chỉ Có Thể Nhập 1 Hoặc 2" 
         ;;
