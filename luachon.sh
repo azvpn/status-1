227d76bfc9d8847188d4e5de6eeed833b6f92d1e
binhvui06 <87931707+binhvui06@users.noreply.github.com>
2022-05-24 18:47:12 +0800

    Update luachon.sh

--- luachon.sh
+++ luachon.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "1. WEB LONG                                                       2.File 2"
+echo "1. WEB BINH                                                       2.File 2"
 
 echo && read -p "  Vui Lòng Nhập Một Lựa Chọn 1 Hoặc 2: " num
 
     case "${num}" in
-        1) bash <(curl -Ls https://raw.githubusercontent.com/azvpn/xrayr/main/install.sh)
+        1) bash <(curl -Ls https://raw.githubusercontent.com/binhvui06/xrayr/main/install.sh)
         ;;
         2) echo "Không Có Gì"
         ;;
